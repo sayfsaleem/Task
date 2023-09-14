1eea731f38bf8253d4422d746cf747ffb0c6400c
sayfsaleem <114619134+sayfsaleem@users.noreply.github.com>
2023-09-14 12:18:02 +0500

    Update settings.py

--- Task/Task/settings.py
+++ Task/Task/settings.py
@@ -148,3 +148,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
